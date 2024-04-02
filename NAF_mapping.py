a0aefdc14ff16301cccd5c01213c09c2887982f0
nrandriamanana <nathan.randriamanana@eleve.ensai.fr>
2024-04-02 10:23:02 +0000

    add filter in  one to many'

--- NAF_mapping.py
+++ NAF_mapping.py
@@ -6,23 +6,28 @@ from lxml import etree
 excel_file_path = "Table de correspondances NAF rev.2 - NAF 2025.xls"
 
 # Import as dataframe
-correspondance_NAF = pd.read_excel(excel_file_path, sheet_name='à compléter')
-#print(correspondance_NAF.columns.values)
+correspondance_NAF = pd.read_excel(excel_file_path, sheet_name='0) (a completer)')
+print(correspondance_NAF.columns.values)
 # Select columns
-correspondance_NAF = correspondance_NAF[['NAFold-code à compléter',
-                                        'NAFold-intitulé à compléter',
-                                        'NAFnew-code à compléter', 
-                                        'NAFnew-intitulé à compléter']]
+correspondance_NAF = correspondance_NAF[['NAFold-code (a completer)',
+                                        'NAFold-intitulé (a completer)',
+                                        'NAFnew-code (a completer)', 
+                                        'NAFnew-intitulé (a completer)',
+                                        'ligne à supprimer =1 (a completer))']]
 
 # Drop the first row (index 0)
 correspondance_NAF = correspondance_NAF.drop(0)
 
 # Rename the column
-correspondance_NAF.rename(columns={'NAFold-code à compléter': 'NAF2008', 
-                                   'NAFold-intitulé à compléter': 'NAF2008_intitule', 
-                                   'NAFnew-code à compléter': 'NAF2025', 
-                                   'NAFnew-intitulé à compléter'}, inplace=True)
+correspondance_NAF.rename(columns={'NAFold-code (a completer)': 'NAF2008', 
+                                   'NAFold-intitulé (a completer)': 'NAF2008_intitule', 
+                                   'NAFnew-code (a completer)': 'NAF2025', 
+                                   'NAFnew-intitulé (a completer)' : 'NAF2025_intitule',
+                                   'ligne à supprimer =1 (a completer)' : 'filtre_a_supp'}, inplace=True)
 
+# Filter
+correspondance_NAF = correspondance_NAF[correspondance_NAF['filtre_a_supp'] != 1]
+print(correspondance_NAF)
 # Delete .
 correspondance_NAF['NAF2008'] = correspondance_NAF['NAF2008'].str.replace(".", "")
 correspondance_NAF['NAF2025'] = correspondance_NAF['NAF2025'].str.replace(".", "")

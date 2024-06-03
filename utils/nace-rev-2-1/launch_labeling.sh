ce4aabb671dca31c29101b31905a33e8523212c7
nrandriamanana <nathan.randriamanana@eleve.ensai.fr>
2024-06-03 17:32:40 +0000

    test site

---
+++ utils/nace-rev-2-1/launch_labeling.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+#set -x
+
+./labeling_pipeline_s3.sh 
+./batch_AGRI.sh 
+./batch_CG.sh 
+./batch_PSA.sh 
+./batch_RSV.sh 
+./batch_SOCET.sh 
+./batch_SSP.sh 
+echo $S3_BUCKET_PREFIX_DEPOT_MANUEL

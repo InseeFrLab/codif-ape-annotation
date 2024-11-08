3a96f2988698551efa733105ef53574a93af7e46
Nathan Randriamanana <37664429+TheAIWizard@users.noreply.github.com>
2024-11-08 12:50:05 +0100

    Update launch_labeling.sh

--- utils/nace-rev-2-1/launch_labeling.sh
+++ utils/nace-rev-2-1/launch_labeling.sh
@@ -6,9 +6,9 @@ pip install -r requirements.txt
 
 # launch scripts
 . ./labeling_pipeline_s3.sh 
-#. ./batch_AGRI.sh 
+. ./batch_AGRI.sh 
 . ./batch_CG.sh 
-#. ./batch_PSA.sh 
-#. ./batch_RSV.sh 
-#. ./batch_SOCET.sh 
-#. ./batch_SSP.sh 
+. ./batch_PSA.sh 
+. ./batch_RSV.sh 
+. ./batch_SOCET.sh 
+. ./batch_SSP.sh 
